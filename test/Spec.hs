34c7851a34a3b86d1a0f356d8fea11ca5c522894
Ryan Bernstein <ryanbernstein1@gmail.com>
2017-06-14 13:40:38 -0400

    split library/executable, set up Tasty for testing

---
+++ test/Spec.hs
@@ -0,0 +1,32 @@
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Test.Tasty.HUnit
+
+import Data.List
+import Data.Ord
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties, unitTests]
+
+properties = testGroup "Properties"
+  [ testProperty "sort == sort . reverse" $
+      \list -> sort (list :: [Int]) == sort (reverse list)
+  , testProperty "Fermat's little theorem" $
+      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
+  -- the following property does not hold
+  , testProperty "Fermat's last theorem" $
+      \x y z n ->
+        (n :: Integer) >= 3 ==> x^n + y^n /= (z^n :: Integer)
+  ]
+
+unitTests = testGroup "Unit tests"
+  [ testCase "List comparison (different length)" $
+      [1, 2, 3] `compare` [1,2] @?= GT
+
+  -- the following test does not hold
+  , testCase "List comparison (same length)" $
+      [1, 2, 3] `compare` [1,2,2] @?= LT
+  ]
+
