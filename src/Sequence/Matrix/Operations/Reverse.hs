5b0cae5c14ec56b311ab14bb0338ce2a2ab6be76
Ryan Bernstein <ryanbernstein1@gmail.com>
2017-08-24 17:08:41 -0400

    removed old reverse

--- src/Sequence/Matrix/Operations/Reverse.hs
+++ src/Sequence/Matrix/Operations/Reverse.hs
@@ -27,39 +27,3 @@ reverseSequence s = s {
         endsTrans' = fromCols [mainStart]
 
         trans' = joinTransTokens (mainStart', mainTrans', endsStart, endsTrans')
-
-dropoutDist :: [Prob] -> [Prob]
-dropoutDist [] = []
-dropoutDist (p:rest) = p : dropoutDist (map (/ (1 - p)) rest)
-
--- NOT SURE IF backward WORKS CORRECTLY. really should take more time to test it.
-priors m = flip M.vecIns (1, 1.0)
-            . flip M.vecIns (M.width m, 1.0)
-            . ((/ fromIntegral nTimes) <$>)
-            . Prelude.sum
-            . Prelude.take (M.height m * nTimes)
-            . (\m' -> iterate (`M.mulVM` m') (onehotVector 1 (M.height m)))
-            . M.replaceRow (onehotVector 1 (M.width m)) (M.height m)
-            $ m
-  where nTimes = 100
-
-reverseT ::
-  (Eq a, Fractional a) => M.SparseMatrix a -> M.SparseMatrix a
-reverseT m = normalize
-             . flip M.ins ((1,1), 0)
-             . flip M.ins ((M.height m, M.width m), 1.0)
-             . mapWithIxs bayes
-             $ M.trans flipped
-  where ps' = priors m
-        ps = reverseVec ps'
-
-        flipped = reverseRows . reverseCols $ m
-        --flipped = reverseRows . reverseCols . snd . M.popRow (M.height m) $ m
-        bayes (r, c) val = (ps M.! r) * val / (ps M.! c)
-
-reverseSequence' :: MatSeq s -> MatSeq s
-reverseSequence' s = MatSeq {
-    trans = let m = getNormalTrans s
-            in snd . popCol 1 . snd . M.popRow (M.height m) $ reverseT m
-  , stateLabels = V.reverse (stateLabels s)
-  }
