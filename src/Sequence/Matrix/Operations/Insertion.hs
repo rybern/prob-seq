e31f188319dc0db620ee4f1772ee3c2657e480f5
Ryan Bernstein <ryanbernstein1@gmail.com>
2017-08-26 11:08:32 -0400

    first whack at insert, totally untested

--- src/Sequence/Matrix/Operations/Insertion.hs
+++ src/Sequence/Matrix/Operations/Insertion.hs
@@ -16,8 +16,52 @@ import qualified Data.Set as Set
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+import Sequence.Matrix.Operations.AndThen
+
+-- what if the state points to itself, and the delim has skips? it could loop to itself, like geometricRepeat
+  -- One cheap way to avoid this is to use the original distribution for looping back - but it's not pretty. That would mean skip 0 ends can then restart the delim, but skip 1 couldn't, so skip doesn't behave as normally defined
+-- to summarize: a dist that skips won't work as expected with a stationary state
+
+-- the 'end' of the delim represents returning control to the original. does that mean you can never insert a chance of ending the sequence? how limiting is that?
 insert :: MatSeq s -> Int -> MatSeq s -> MatSeq s
-insert seq ix delim = seq
+insert seq 0 delim = delim `andThen` seq
+insert seq ix delim =
+  if ix < 0 || ix > V.length (stateLabels seq)
+  then seq
+  else MatSeq {
+    trans = trans'
+  , stateLabels = stateLabels'
+  }
+  where stateLabels' = stateLabels seq <> stateLabels delim
+        nStates = V.length (stateLabels seq)
+        nStates' = V.length stateLabels'
+        nNewStates = V.length (stateLabels delim)
+
+        (delimMainStart, delimMainTrans, delimEndsStart, delimEndsTrans) = splitTransTokens (trans delim)
+        (seqMain, seqEnds) = splitEnds (trans seq)
+        stateRowMain = M.row seqMain (ix + 1)
+        expSeqMain = M.hconcat [seqMain, M.zeroMx (nStates, nNewStates)]
+
+        -- weighted sum of convolving delimEnds with each target, in unexpanded seq
+        -- how to deal with ends?
+        translateDelimEnds delimEnds = distributeEndDistAtState (trans seq) ix stateRowMain
+
+        stateProbNotEnd = (1 -) . sum $ M.row seqEnds (ix + 1)
+        stateRowMain' =  translateDelimEnds delimEndsStart <> ((stateProbNotEnd *) <$> delimMainStart)
+
+        expSeqMain' = M.replaceRow stateRowMain' (ix + 1) expSeqMain
+
+        delimLeft = mapRows translateDelimEnds delimEndsTrans
+
+        seqMain' = M.vconcat [ expSeqMain'
+                             , M.hconcat [delimLeft, delimMainTrans]]
+        seqEnds' = M.vconcat [ seqEnds
+                             , M.zeroMx (fst $ M.dims seqEnds, nNewStates)]
+        trans' = M.hconcat [seqMain', seqEnds']
+
+distributeEndDistAtState :: Trans -> Int -> Dist -> Dist
+distributeEndDistAtState trans ix end = distributeEndDist trans' end
+  where trans' = M.replaceRow (M.row trans (ix + 1)) 1 trans
 
 
 {-
