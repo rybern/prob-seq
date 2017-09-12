197ff84a38ead530364a304e4273785a7cad9f17
Ryan Bernstein <ryanbernstein1@gmail.com>
2017-09-12 14:41:43 -0400

    definitely need attoparsec for matrix.IO, 'Read' is not sufficient or desirable

--- src/Sequence/Matrix/IO/TransMatrix.hs
+++ src/Sequence/Matrix/IO/TransMatrix.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedLists, ViewPatterns #-}
 module Sequence.Matrix.IO.TransMatrix where
 
 import qualified Math.LinearAlgebra.Sparse as M
@@ -7,19 +8,31 @@ import Data.List.Split
 import System.IO
 import System.FilePath.Posix
 
---import Sequence.Matrix.Operations
 import Sequence.Matrix.Types
 import Sequence.Matrix.ProbSeqMatrixUtils
+
+import Sequence.Matrix.Operations hiding (intersperse)
 
 -- .st files should be r = c+1 with r0 startdist
 -- .st files should be internal representation
 
 -- getTrans isn't exactly right
 
-writeSTFile :: (Show a) => FilePath -> MatSeq a -> IO ()
-writeSTFile = writeSeqToFile (addFixedEndRow . collapseEnds . trans) "st"
+test = andThen
+  (deterministicSequence ["one", "two", "three"])
+  (eitherOr 0.8
+    (deterministicSequence ["four", "five", "six"])
+    (reverseSequence
+      (collapse (V.foldl1 (++)) 2
+        (deterministicSequence ["seven", "eight", "nine"]))))
 
-writeSTPFile :: FilePath -> MatSeq Char -> IO ()
+stTrans :: (MatSeq a -> M.SparseMatrix Prob)
+stTrans = addFixedEndRow . collapseEnds . trans
+
+writeSTFile :: (Show a) => FilePath -> MatSeq a -> IO ()
+writeSTFile = writeSeqToFile stTrans "st"
+
+writeSTPFile :: (Show a) => FilePath -> MatSeq a -> IO ()
 writeSTPFile = writeSeqToFile trans "stp"
 
 writeSeqToFile :: (Show a)
@@ -28,7 +41,17 @@ writeSeqToFile :: (Show a)
                -> FilePath
                -> MatSeq a
                -> IO ()
-writeSeqToFile t extension fp seq = withFile (fp ++ "." ++ extension) WriteMode $ \h -> do
+writeSeqToFile t extension fp seq = withFile dest WriteMode $ writeSeq t seq
+  where dest = if takeExtension fp == extension
+               then fp
+               else fp `addExtension` extension
+
+writeSeq :: (Show a)
+         => (MatSeq a -> M.SparseMatrix Prob)
+         -> MatSeq a
+         -> Handle
+         -> IO ()
+writeSeq t seq h = do
 
   let (labels, tags) = V.unzip $ stateLabels seq
 
@@ -46,10 +69,6 @@ writeSeqToFile t extension fp seq = withFile (fp ++ "." ++ extension) WriteMode 
   mapM_ (hPutStrLn h) . map showElem . tail . M.toAssocList $ matrix
   where matrix = t seq
         showElem ((r, c), val) = show (pred r) ++ " " ++ show (pred c) ++ " " ++ show (fromRational val :: Double)
-        fp' = if takeExtension fp == extension
-              then fp
-              else fp `addExtension` extension
-
 
 readSTFile :: (Read a)
            => FilePath
@@ -67,9 +86,12 @@ readSeqFromFile :: (Read a)
                 -> FilePath
                 -> IO (MatSeq a)
 readSeqFromFile seqFn fp = do
-  ((_:labelHeader:tagHeader:_):matLines) <- map words . lines <$> readFile fp
+  ((_:labelHeader):(_:tagHeader):matLines) <- lines <$> readFile fp
 
-  let lineAssocList [r, c, val] = ((succ (read r), succ (read c)), read val)
+  putStrLn labelHeader
+  putStrLn tagHeader
+
+  let lineAssocList (words -> [r, c, val]) = ((succ (read r), succ (read c)), read val)
       matrix = M.fromAssocList $ map lineAssocList matLines
       labels = V.map read . V.fromList $ splitOn "," labelHeader
       tags = V.map read . V.fromList $ splitOn "," tagHeader
